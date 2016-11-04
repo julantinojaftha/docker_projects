15e2cc11c39f29384ab1c62a8ea7f88cd531905c
julantinojaftha <julantino@Julans-MacBook-Pro.local>
2016-11-04 16:11:10 +0200

    Fixed bugs with configure file

---
+++ bittorrent/create_directories.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir /media/USBHDD1
+mkdir /media/USBHDD1/shares
+mkdir /media/USBHDD1/shares/torrents
+mkdir /media/USBHDD1/shares/torrents/downloading
+mkdir /media/USBHDD1/shares/torrents/completed
+mkdir /media/USBHDD1/shares/torrents/watch
+mkdir /media/USBHDD1/shares/torrents/torrent-backups
+mkdir /media/USBHDD1/shares/torrents/settings
--- bittorrent/run.sh
+++ bittorrent/run.sh
@@ -6,13 +6,15 @@ while [ ! -f ~/.config/deluge/auth ] ;
 do
   echo "Waiting for auth file to be created";
 done
+sleep 2
 
 pkill deluged
 
 cp ~/.config/deluge/auth ~/.config/deluge/auth.old
 echo "root:toor:10" >> ~/.config/deluge/auth
 
-cp /core.conf ~/.config/deluge/core.conf
+#rm ~/.config/deluge/core.conf
+
 
 # Starting to install web interface
 apt-get install -y deluge-web
@@ -25,5 +27,12 @@ do
    pkill deluge-web
 done
 
-pkill deluge-web
+sleep 2
+
+#pkill deluge-web
 python /rs.py ~/.config/deluge
+
+pkill deluged
+pkill deluge-web
+cp /core.conf ~/.config/deluge/core.conf
+
